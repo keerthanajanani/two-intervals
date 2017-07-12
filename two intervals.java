3ff7f3f5da0e44dab25fb5c2aa7dd55f1c660936
keerthanajanani <keerthijanani66@gmail.com>
2017-07-12 10:17:17 +0530

    Create two intervals.java

---
+++ two intervals.java
@@ -0,0 +1,20 @@
+# two-intervals
+import java.io.*
+import java.util.*;
+public class two intervals
+{
+public static void main(String args[])throws IOException
+{
+int n,i;
+BufferedReader br=new BufferedReader(new InputStreamReader(br.readLine());
+System.out.println("Enter the input");
+n=Integer.parseInt(br.readLine());
+for(i=1;i<=100;i++)
+{
+if(i%2==0)
+{
+System.out.println(i);
+}
+}
+}
+}
